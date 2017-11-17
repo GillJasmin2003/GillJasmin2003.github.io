bc4d25faa73e4e952d39d03750c410d3ac519034
GillJasmin2003 <33737006+GillJasmin2003@users.noreply.github.com>
2017-11-16 16:00:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function math() {
 var answer = prompt("Enter 1+1:");
-  <script type="text/javascript" src="main.js"></script>
 if (answer == 2) {
   confirm("You got it right. Your answer was " + answer);
 }
